8a69713bd893888c01c88bdfbd2c6cbe54379e03
avinasin <avinasin@LIN36000226.corp.capgemini.com>
2017-10-22 12:27:27 +0530

    new change

--- src/main/java/com/visualsearch/ws/impl/SearchWsImpl.java
+++ src/main/java/com/visualsearch/ws/impl/SearchWsImpl.java
@@ -3,6 +3,8 @@ package com.visualsearch.ws.impl;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import com.visualsearch.visenze.impl.VisualSearchImpl;
@@ -12,6 +14,7 @@ public class SearchWsImpl {
  
 	@GET
 	@Path("/{param}")
+	@Produces(MediaType.APPLICATION_JSON)
 	public Response getMsg(@PathParam("param") String searchInput) {
 		VisualSearchImpl vs = new VisualSearchImpl();
 				
