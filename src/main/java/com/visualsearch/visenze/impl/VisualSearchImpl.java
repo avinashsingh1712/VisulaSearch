309605e41e049f0be159ff1f010e7b8e5db24fc6
Singh <avinash.c.singh@capgemini.com>
2017-10-23 19:10:47 +0530

    search by url

--- src/main/java/com/visualsearch/visenze/impl/VisualSearchImpl.java
+++ src/main/java/com/visualsearch/visenze/impl/VisualSearchImpl.java
@@ -19,81 +19,112 @@ import com.visenze.visearch.ViSearch;
  *
  */
 public class VisualSearchImpl {
-	
+
 	private static String ACCESS_KEY = "2922ca9709bb6f648b3cc0c95dd25453";
 	private static String SECRET_KEY = "5ff65d3b5b8f5262c82e5fb4b6c1cc79";
 	private static ViSearch client = new ViSearch(ACCESS_KEY, SECRET_KEY);
-	
-	public VisualSearchImpl() {
-		// TODO Auto-generated constructor stub
-	}
-	
+
 	/**
 	 * 
-	 */			
-	public String searchForSimilarRecommendations(String im_name) {	
-		
-				
-		//Set the required filters.
-		SearchParams params = setFilters(im_name);
-		
+	 * @param request
+	 * @return
+	 */
+	public String search(String request) {
+		String response = null;
+		if (!request.contains("http")) {
+			response = searchForSimilarRecommendations(request);
+		} else {
+			response = searchByUploadedImageAndURL(request);
+		}
+		return response;
+	}
+
+	/**
+	 * 
+	 * @param im_name
+	 * @return
+	 */
+	private String searchForSimilarRecommendations(String im_name) {
+
+		// Set the required filters.
+		SearchParams params = setFiltersForSimilarRecommendations(im_name);
+
 		PagedSearchResult searchResult = client.search(params);
-		
+
 		String errorMessage = searchResult.getErrorMessage();
-		if(errorMessage != null) {
+		if (errorMessage != null) {
 			return errorMessage;
 		}
-		
-		System.out.println("Search data list - "+searchResult.getRawJson());
-		//System.out.println("Search image result. - "+searchResult.getResult());
-		
+
+		System.out.println("Search data list - " + searchResult.getRawJson());
+		// System.out.println("Search image result. -
+		// "+searchResult.getResult());
+
 		List<ImageResult> result = searchResult.getResult();
-		for (ImageResult imageResult : result) {			
-			ImageResult ir =imageResult;
-		//	System.out.println(ir.getImName());
+		for (ImageResult imageResult : result) {
+			ImageResult ir = imageResult;
+			// System.out.println(ir.getImName());
 		}
-				
+
 		return searchResult.getRawJson();
 	}
 
-	
-	public String searchByUploadedImage (){
-		//Searching an uploaded image file
+	private String searchByUploadedImageAndURL(String url) {
+		// Searching an uploaded image file
 		File imageFile = new File("C:/Users/Public/Pictures/Sample Pictures/Penguins.jpg");
-		UploadSearchParams params = new UploadSearchParams(imageFile);
-		params.setFacet(true);
-		//System.out.println(params.getImageFile());
-		//PagedSearchResult searchResult = client.uploadSearch(params);
-		//if(searchResult.getErrorMessage()!= null) {
-		//	return searchResult.getErrorMessage();
-		//}
-		//System.out.println(searchResult.getRawJson());
-		
-		
-		//Searching a publicly accessible image URL
-		String url = "http://www.ikea.com/us/en/images/products/billy-bookcase-white__0252367_PE391149_S4.JPG";
-		UploadSearchParams params2 = new UploadSearchParams(url);
-		
+		//String url = "http://www.ikea.com/us/en/images/products/billy-bookcase-white__0252367_PE391149_S4.JPG";
+		UploadSearchParams params = setFiltersForSearchByUploadedImageAndURL(url);
+
+		// params.setDetection(detection)
+		// System.out.println(params.getImageFile());
+		// PagedSearchResult searchResult = client.uploadSearch(params);
+		// if(searchResult.getErrorMessage()!= null) {
+		// return searchResult.getErrorMessage();
+		// }
+		// System.out.println(searchResult.getRawJson());
+
+		// Searching a publicly accessible image URL
+
 		PagedSearchResult searchResult2 = client.uploadSearch(params);
-		System.out.println(params2.getImageUrl());
-		
+		System.out.println(searchResult2.getRawJson());
+
 		return "";
 	}
-	
+
+	/**
+	 * @param url
+	 * @return
+	 */
+	private UploadSearchParams setFiltersForSearchByUploadedImageAndURL(String url) {
+		UploadSearchParams params = new UploadSearchParams(url);
+		params.setFacet(true);
+		params.setGetAllFl(true);
+		params.setLimit(30);
+		params.setPage(1);
+		params.setScore(true);
+		params.setQInfo(true);
+
+		List<String> fl = new ArrayList<String>();
+		fl.add(url);
+		params.setFl(fl);
+		return params;
+	}
 
 	/**
 	 * @param requestData
 	 * @return
 	 */
-	private SearchParams setFilters(String requestData) {
+	private SearchParams setFiltersForSimilarRecommendations(String requestData) {
+
+		SearchParams params = new SearchParams(requestData);
+
 		float scoreMin = 0.60F;
-		float scoreMax = 1.0F;	
-		
+		float scoreMax = 1.0F;
+
 		List<String> fl = new ArrayList<String>();
 		fl.add(requestData);
-		
-		//SearchParams params = new SearchParams("malm2");
-		SearchParams params = new SearchParams(requestData);
+
+		// SearchParams params = new SearchParams("malm2");
 		params.setLimit(30);
 		params.setPage(1);
 		params.setScore(true);
@@ -104,26 +135,23 @@ public class VisualSearchImpl {
 		params.setGetAllFl(true); // to get all the image URLS
 		params.setQInfo(true);// To get the main image URL
 		return params;
-		
-		//curl "http://visearch.visenze.com/search?im_name=malm100&page=1&limit=30&score=true&fl=im_url&dedup=false" \-u access_key:secret_key
 	}
-	
 
-	public static void main (String args[]) {	
+	public static void main(String args[]) {
 		VisualSearchImpl impl = new VisualSearchImpl();
-		impl.searchForSimilarRecommendations("malm100");	
-		//System.out.println(impl.searchByUploadedImage());
+		// impl.searchForSimilarRecommendations("malm100");
+		System.out.println(impl.searchByUploadedImageAndURL(""));
 	}
-	
+
 	/**
 	 * 
 	 * @param uploadSearchParams
 	 * @return
 	 */
-	public PagedSearchResult uploadSearchImages(UploadSearchParams uploadSearchParams){
-		
+	private PagedSearchResult uploadSearchImages(UploadSearchParams uploadSearchParams) {
+
 		ViSearch client = new ViSearch(ACCESS_KEY, SECRET_KEY);
-		
+
 		List<Image> images = new ArrayList<Image>();
 		String imName = "new_upload_IKEA2";
 		String imUrl = "http://www.ikea.com/ms/media/logos/diners.gif";
@@ -131,13 +159,13 @@ public class VisualSearchImpl {
 		// add metadata to your image
 		Map<String, String> metadata = new HashMap<String, String>();
 		metadata.put("category", "IKEA");
-		//metadata.put("description", "A pair of high quality leather wingtips");
-		//metadata.put("price", "100.0");
+		// metadata.put("description", "A pair of high quality leather
+		// wingtips");
+		// metadata.put("price", "100.0");
 		images.add(new Image(imName, imUrl, metadata));
 		client.insert(images);
-		
+
 		return null;
 	}
-	
-		
+
 }
--- src/main/java/com/visualsearch/ws/impl/SearchWsImpl.java
+++ src/main/java/com/visualsearch/ws/impl/SearchWsImpl.java
@@ -1,5 +1,6 @@
 package com.visualsearch.ws.impl;
- 
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -8,18 +9,28 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import com.visualsearch.visenze.impl.VisualSearchImpl;
- 
+
 @Path("/searchByImage")
 public class SearchWsImpl {
- 
+
 	@GET
 	@Path("/{param}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response getMsg(@PathParam("param") String searchInput) {
+	public Response getSearchResult(@PathParam("param") String searchInput) {
 		VisualSearchImpl vs = new VisualSearchImpl();
-				
-		return Response.status(200).entity(vs.searchForSimilarRecommendations(searchInput)).build();
- 
+
+		return Response.status(200).entity(vs.search(searchInput)).build();
+
 	}
- 
+
+	@Path("/JPG")
+	@Consumes({ MediaType.APPLICATION_JSON })
+	@Produces({"image/jpeg,image/png"})
+	public Response createProductInJSON(String url) {
+		VisualSearchImpl vs = new VisualSearchImpl();
+		//String result = "Product created : " + product;
+		return Response.status(201).entity(vs.search(url)).build();
+
+	}
+
 }
