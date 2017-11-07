f27f541bd42cac77b6ac57c20b003725eed214b3
Avinash <32963836+avsin3@users.noreply.github.com>
2017-11-07 12:34:51 +0100

    Update index.js

--- src/main/webapp/WEB-INF/js/index.js
+++ src/main/webapp/WEB-INF/js/index.js
@@ -1,43 +1,16 @@
 function loadMain() {
+    console.log("imageuoloD");
+	var inputImageFile = document.getElementById("file-input");
 	var maindiv = document.getElementById("mainDiv");
 	var preview = document.getElementById("preview");
 	var name = $("#searchName").val();
-	$.ajax({
-		url : "search/searchByImage/" + name,
-		success : function(response) {
-			console.log(response);
-			$.each(response, function(key, value) {
-				if (key == "qinfo") {
-					var iDiv1 = document.createElement('div');
-					iDiv1.className = 'inline';
-					var text12 = document.createElement('p');
-					var text11 = document.createElement('p');
-					var oImg1 = document.createElement("img");
-
-					$.each(value, function(k1, v1) {
-						if (k1 == "im_name") {
-							console.log("name   " + value.im_name);
-							text12.innerHTML = value.im_name;
-						}
-						if (k1 == "category") {
-							console.log("category   " + value.category);
-							text11.innerHTML = v1;
-						}
-						if (k1 == "im_url") {
-							console.log("im_url   " + value.im_url);
-							oImg1.setAttribute('src', v1);
-							oImg1.setAttribute('alt', value.im_name);
-							oImg1.setAttribute("class", "mainImageCont");
-						}
-					});
-					iDiv1.appendChild(oImg1);
-					iDiv1.appendChild(text12);
-					iDiv1.appendChild(text11);
-					preview.innerHTML = "";
-					preview.appendChild(iDiv1);
-					
-				}
-
+    console.log("imageuoloD"+ inputImageFile);
+    visearch.uploadsearch({
+    	  image: inputImageFile,
+    	  fl: ["im_url"]
+    	}, function(res) {
+    		console.log(res);
+    		$.each(res, function(key, value) {
 				if (key == "result") {
 					mainDiv.innerHTML = "";
 					var product_left1 = document.createElement('div');
@@ -47,12 +20,6 @@ function loadMain() {
 					pTag.className = 'preview_left_cont';
 					product_left1.appendChild(pTag);
 					left2.innerHTML = "";
-					var radioInput = document.createElement('input');
-			        radioInput.setAttribute('type', 'radio');
-			        var radioInput2 = document.createElement('input');
-			        radioInput2.setAttribute('type', 'radio');
-			        product_left1.appendChild(radioInput);
-			        product_left1.appendChild(radioInput2);
 					left2.appendChild(product_left1);
 					
 						$.each(value, function(key1, value1) {
@@ -83,11 +50,11 @@ function loadMain() {
 							}
 							if (k == "score") {
 								console.log("score  " + value1.score);
-								
+								//text2.innerHTML = v.toFixed(3);
 								iDiv.addEventListener('mouseover', mouseOver);
 								  iDiv.addEventListener('mouseout', mouseOut);
 								  function mouseOver() {
-									  text2.innerHTML = v.toFixed(3);
+									  text2.innerHTML = v;
 									  iDiv.appendChild(text2);
 								   
 								  }
@@ -142,12 +109,9 @@ function loadMain() {
 					});
 
 				}
-				
-
 			});
-		}
-	});
-
+    	
+    	});
+	
 }
-
 
