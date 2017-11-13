8df3521f29b0f4256213cfd098fc311023266118
Avinash <32963836+avsin3@users.noreply.github.com>
2017-11-13 08:34:16 +0100

    Update preview.js

--- src/main/webapp/js/preview.js
+++ src/main/webapp/js/preview.js
@@ -1,28 +1,26 @@
-$(function () {
-    $("#file-input").change(function () {
-        $("#preview").html("");
-        $("#mainDiv").html("");
-        var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.jpg|.jpeg|.gif|.png|.bmp)$/;
-        if (regex.test($(this).val().toLowerCase())) {
-            if ($.browser.msie && parseFloat(jQuery.browser.version) <= 9.0) {
-                $("#preview").show();
-                $("#preview")[0].filters.item("DXImageTransform.Microsoft.AlphaImageLoader").src = $(this).val();
-            }
-            else {
-                if (typeof (FileReader) != "undefined") {
-                    $("#preview").show();
-                    $("#preview").append('<div class="product_holder1"><img/><div id="more_container"><input id="more" type="button" value="Search Similar" onclick="loadMain();" /></div></div>');
-                    var reader = new FileReader();
-                    reader.onload = function (e) {
-                        $("#preview img").attr("src", e.target.result).css({"height": "220px","width": "400px","padding":"22px","background":"gray"});
-                    }
-                    reader.readAsDataURL($(this)[0].files[0]);
-                } else {
-                    alert("This browser does not support FileReader.");
-                }
-            }
-        } else {
-            alert("Please upload a valid image file.");
+ $(function () {
+
+      // Initialise CropSelect
+      $('#preview').CropSelectJs();
+
+
+      // Handle file select change
+      $('#file-input').on('change', function() {
+    	  /*var tmppath = URL.createObjectURL(event.target.files[0]); 
+    	  console.log("hell" +tmppath);*/
+    	  
+    	  //$("#preview").append('<div class="product_holder1"><img/><div id="more_container"><input id="more" type="button" value="Search Similar" onclick="loadMain();" /></div></div>');
+    	  $("#preview1").append('<input id="more" type="button" value="Search Similar" onclick="loadMain();"/>');
+        if (this.files && this.files[0]) {
+		
+          var reader = new FileReader();
+          reader.onload = function (e) {
+            $('#preview').css("border","7px solid #676565").CropSelectJs('setImageSrc', e.target.result);
+			
+          };
+          reader.readAsDataURL(this.files[0]);
         }
+      });
+
+
     });
-})
