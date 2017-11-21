c5a325bda76764bcba81e8ec33f3c0e1836be9af
avsin3 <avsin3@ITINBAC-DW0760.ikea.com>
2017-11-21 15:13:50 +0100

    url

--- src/main/webapp/js/visualSearch_main.js
+++ src/main/webapp/js/visualSearch_main.js
@@ -32,6 +32,7 @@ function loadMain() {
         
                 
         var xhr = new XMLHttpRequest();
+        //var url = "http://itinbac-dw0760:8080/VisualSearch/search/imageSearch/searchByImage";
         var url = "search/imageSearch/searchByImage";
         xhr.open("POST", url, true);
         xhr.setRequestHeader("Content-type", "application/json");
